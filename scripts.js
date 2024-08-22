312eab8b7c7bab7f30815c7835b1fc550c11d587
DHyyppa <80334374+DHyyppa@users.noreply.github.com>
2024-08-22 14:15:03 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -255,7 +255,7 @@ class Chatbot {
     const chatBox = document.getElementById('chat-box');
     chatBox.innerHTML = '';
     this.threadId = ''; // Reset the thread ID
-    this.addWelcomeMessage();
+    this.sendWelcomeMessage();
   }
 
   downloadMessages() {
