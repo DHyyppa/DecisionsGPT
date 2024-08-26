3d6a1f14d96ca0a3c965ae0231634c9d90b0148c
DHyyppa <80334374+DHyyppa@users.noreply.github.com>
2024-08-26 08:35:33 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 class Chatbot {
   constructor() {
     this.threadId = ''; // Initialize with an empty thread ID
-    this.assistantId = "asst_ALdJUvFJqCgXSXNHmHjEVutb";
+    this.assistantId = "asst_naTC6ByqvURKXMJqrJRRsveC";
     this.baseUrl = "internal.decisions.com";
     this.nameSpace = "NS-08daf277-99f3-2854-1c3c-e50e30089599";
     this.addEventListeners();
